7a92a77afe4bb51febd0e65e2135a09e7ac0769d
Daniel Owen van Dommelen <theapemachine@gmail.com>
2026-09-20 22:08:34 +0200

    fixes

--- pkg/network/ready.go
+++
@@ -1,8 +0,0 @@
-package network
-
-import "context"
-
-// ReadyTransport is implemented by transports that have a blocking initialization phase.
-type ReadyTransport interface {
-	Ready(ctx context.Context) error
-}
